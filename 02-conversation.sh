08931ca4121a2626be8e8501815c0dfb1840e07d
MMahiketh <mahi@github.com>
2024-09-06 23:09:16 +0530

    Learn echo

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh: Hi, Suresh!"
+echo "Suresh: Hi, Ramesh! How are you?"
+echo "Ramesh: I'm good. What about you?"
+echo "Suresh: I'm fine. What are you doing?" 
+echo "Ramesh: I'm learing Shell Scripting."
