7f1c76a49739c8cb129598f661b91680160d3e1f
MMahiketh <mahi@github.com>
2024-09-06 23:20:14 +0530

    Learned to use Variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi, $PERSON2!"
+echo "$PERSON2: Hi, $PERSON1! How are you?"
+echo "$PERSON1: I'm good. What about you?"
+echo "$PERSON2: I'm fine. What are you doing?" 
+echo "$PERSON1: I'm learing Shell Scripting."
