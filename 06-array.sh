20c2692db5f23362e4ddac441f0585e8938b0cbf
MMahiketh <mahi@github.com>
2024-09-07 11:09:57 +0530

    Learned more about arrays

--- 06-array.sh
+++ 06-array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-STUDENTS=($@)
+read -p "Enter student's name:	" -a STUDENTS
 
-echo "First person name: ${STUDENTS[0]}"
-echo "Second person name: ${STUDENTS[1]}"
-echo "Third person name: ${STUDENTS[2]}"
-echo "All person's names: ${STUDENTS[@]}"
+echo "First student name: ${STUDENTS[0]}"
+echo "Second student name: ${STUDENTS[1]}"
+echo "Third student name: ${STUDENTS[2]}"
+echo "All student's names: ${STUDENTS[@]}"
