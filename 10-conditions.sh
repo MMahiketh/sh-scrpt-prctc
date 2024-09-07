c2ccc6f03ad573e1789276b37a04aa1d38fbe053
MMahiketh <mahi@github.com>
2024-09-07 18:46:12 +0530

    Learned to use if else conditions

---
+++ 10-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+AGE=$1
+
+if [ $AGE -lt 18  ]
+then
+	echo "Your age is less than 18. Not allowed to vote."
+elif [ $AGE -eq 18 ]
+then
+	echo "Congratulations on your first vote."
+else
+	echo "Your age is greater than 18. You can vote."
+fi
