fb5f6381b3060572f567caa9dccd04d51aea7723
MMahiketh <mahi@github.com>
2024-09-07 20:14:58 +0530

    Learned to install packages using script

--- 11-installing-packages.sh
+++ 11-installing-packages.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-#check if current user is root user
-
-USER_ID=$(id -u)
-
-echo "$USER_ID"
-
-if [ $USER_ID -ne 0 ]
-then
-	echo "Current user is not the root user."
-else
-	echo "Root user detected."
-fi
-
-
-
-#check if package is already installed or not
-
+	#check if package is already installed or not
 dnf list installed git
 
-	#check if above command exec successfully
+		#check if above command exec successfully
 if [ $? -eq 0 ]
 then
 	echo "Package already installed"
+	exit 0
 else
 	echo "Package not found in installed list"
 
-#if not installed, install the package
-	dnf install git
+	#if not installed, install the package
+		#check if current user is root user
+	echo "Verifying if user is root user"
+	
+	USER_ID=$(id -u)
+	echo "$USER_ID"
 
+	if [ $USER_ID -ne 0 ]
+	then
+		echo "Current user is not the root user."
+	else
+		echo "Root user detected."
+		dnf install git
+	fi
 fi
 
 #check if package is installed correctly
+
 dnf list installed git
 
 if [ $? -eq 0 ]
