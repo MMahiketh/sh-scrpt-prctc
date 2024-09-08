4f1c58dc2667ab3d05995a7a8aefa2bb87a5c818
MMahiketh <mahi@github.com>
2024-09-08 12:04:04 +0530

    Learned to install mutiple packages

--- 15-install-multipkg.sh
+++ 15-install-multipkg.sh
@@ -6,20 +6,6 @@ Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
 
-#Function for checking installed packages
-
-CHECK_PKG (){
-
-	dnf list installed $1
-		#check if above command exec successfully
-	if [ $? -eq 0 ]
-	then
-		echo -e "$B $1 $N already installed... $Y SKIPPING $N"	#term+log
-		exit 0
-	else
-		echo -e "$B $1 $R not found $N in installed packages"	#log
-	fi
-}
 
 CHECK_USER(){
 
@@ -53,6 +39,23 @@ INSTALL_PKG(){
 	fi
 }
 
+#Function for checking installed packages
+
+CHECK_PKG (){
+
+	dnf list installed $1
+		#check if above command exec successfully
+	if [ $? -eq 0 ]
+	then
+		echo -e "$B $1 $N already installed... $Y SKIPPING $N"	#term+log
+	else
+		echo -e "$B $1 $R not found $N in installed packages"	#log
+		echo "Trying to install... $PACKAGE"
+			#if not installed, install the package
+		INSTALL_PKG $1
+	fi
+}
+
 USAGE(){
 
 	if [ $# -eq 0 ]
@@ -67,24 +70,14 @@ USAGE(){
 PACKAGES=$@
 
 
-for i in $PACKAGES
-do
-	echo $i
-done
-
 #Usage instructions
 USAGE $PACKAGES
 
-	#Checking for root user
+#Checking for root user
 CHECK_USER
 
 for PACKAGE in $PACKAGES
 do
-	#check if package is already installed or not
+	#check if package is already installed. If not install it.
 	CHECK_PKG $PACKAGE
-
-	#if not installed, install the package
-	echo "Trying to install... $PACKAGE"
-
-	INSTALL_PKG $PACKAGE
 done
