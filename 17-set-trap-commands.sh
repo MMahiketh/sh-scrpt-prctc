c79d608ecadce3766ea26dd332c212390faca9cc
MMahiketh <mahi@github.com>
2024-09-10 13:03:14 +0530

    Learned to use set and trap commands

--- 17-set-trap-commands.sh
+++ 17-set-trap-commands.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+failure(){
+
+	echo "Failed at line:$1 and command is $2 with exit code $? ."
+}
+
+trap 'failure "$LINENO" "$BASH_COMMAND"' ERR
+
 echo "hello from script."
 
 lss -halt
