21fc6b569391b72b1b4f493885682f1300c48b7f
MMahiketh <mahi@github.com>
2024-09-10 17:17:01 +0530

    Learned to check disk usage and send alerts

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -6,17 +6,17 @@ Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
 
-DISK_USAGE=$(df -hT|grep "xfs"|awk -F " " '{print $(NF-1)},$NF}'|cut -d "%" -f1) 
+DISK_USAGE=$(df -hT|grep "xfs"|awk -F " " '{print $(NF-1),$NF}'|cut -d "%" -f1) 
 DISK_THRESHOLD=5
 
 while IFS= read -r disks
 do
-	DISK=$($disks|cut -d " " -f1)
-	USED=$($disks|cut -d " " -f2)
+	DISK=$($disks|cut -d " " -f2)
+	USED=$($disks|cut -d " " -f1)
 	echo "disk is $DISK and used is $USED"
-	#if [ $($disks|cut -d " " -f2) -ge $DISK_THRESHOLD ]
-	#then
-	#	echo "$($disks|cut -d " " -f1) is more than ${DISK_THRESHOLD}%. Current usages is $($disks|cut -d " " -f1)%"
-	#fi
+	if [ $($disks|cut -d " " -f1) -ge $DISK_THRESHOLD ]
+	then
+		echo "$($disks|cut -d " " -f2) is more than ${DISK_THRESHOLD}%. Current usages is $($disks|cut -d " " -f1)%"
+	fi
 	#echo "$($disks|cut -d " " -f2)"
 done <<< $DISK_USAGE
